fe5e67f343b89c7bf7c856e6317c041c9f386a88
Abid Dar <abiddar1979@gmail.com>
2024-05-08 09:25:22 +0500

    files added

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+/*Task # 20 : Think of something you could store in a array. For example , you could
+make a list of mountains,river,countries,cities,languages, or anything els you
+you'd like.Write a program that creates a list contaning these items.*/
+const countries = ["Pakistan", "Brazil", "Canada", "Holland"];
+console.log(`\n List of Countries:`);
+for (let i = 0; i < countries.length; i++) {
+    console.log(countries[i]);
+}
---
+++ app.ts
@@ -0,0 +1,11 @@
+/*Task # 20 : Think of something you could store in a array. For example , you could
+make a list of mountains,river,countries,cities,languages, or anything els you
+you'd like.Write a program that creates a list contaning these items.*/
+
+const countries: string[] = ["Pakistan","Brazil","Canada","Holland"];
+
+console.log(`\n List of Countries:`);
+
+for (let i=0 ; i < countries.length ; i++){
+    console.log(countries[i]);
+}
